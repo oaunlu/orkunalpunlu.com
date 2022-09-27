aad3e9afa7531b2af72b2c043edbebaaf5040a06
Orkun Alp Ünlü <orkunalp98@gmail.com>
2022-09-27 18:30:51 +0300

    Initial commit for portfolio site

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 
+// https://stackoverflow.com/questions/59475858/next-js-single-page-application
+// consider wix and wordpress alternatives
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
